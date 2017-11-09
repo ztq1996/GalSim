639cbb6789749a4c771a9f6c58de8101f21b07ac
Joe Zuntz <joezuntz@googlemail.com>
2017-11-09 08:38:10 +0000

    Auto-updated documention

--- search/properties_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['beta',['beta',['../classgalsim_1_1shear_1_1_shear.html#aade896b9f4c27ff93d1dd8ede67110df',1,'galsim::shear::Shear']]]
-];
--- search/properties_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['shear',['shear',['../classgalsim_1_1shear_1_1_shear.html#ade3f3801e894bccb6c00e5a27dbb46ff',1,'galsim::shear::Shear']]]
-];
